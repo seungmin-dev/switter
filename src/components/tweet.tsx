baf3d71af62f2466976c44f5e77e32871e07cfcc
seungmin-dev <seungmin.dev@gmail.com>
2023-09-25 15:43:20 +0900

    style: 이미지 재업로드 버튼 수정

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -132,7 +132,23 @@ export default function Tweet({ tweet, username, photo, userId, id }: ITweet) {
       {!photo ? null : edit ? (
         <S.EditPhotoBox>
           <S.EditPhotoButton htmlFor="editFile">
-            {editPhoto ? "✅" : "재업로드"}
+            {editPhoto ? (
+              "✅"
+            ) : (
+              <svg
+                style={{ width: "30px" }}
+                fill="#317a23"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm4.75 11.25a.75.75 0 001.5 0v-2.546l.943 1.048a.75.75 0 101.114-1.004l-2.25-2.5a.75.75 0 00-1.114 0l-2.25 2.5a.75.75 0 101.114 1.004l.943-1.048v2.546z"
+                />
+              </svg>
+            )}
           </S.EditPhotoButton>
           <S.AttachEditFileInput
             onChange={onEditFileChange}
