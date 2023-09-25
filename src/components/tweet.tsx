2672bba8d9e20934e928a19efb2a1c660d80e46e
seungmin-dev <seungmin.dev@gmail.com>
2023-09-25 15:40:25 +0900

    Feat: 게시글, 이미지 수정기능 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,11 +1,23 @@
 import { ITweet } from "./timeline";
 import * as S from "../styles/tweet.styles";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { auth, database, storage } from "../firebase";
-import { deleteObject, ref } from "firebase/storage";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import { ChangeEvent, useState } from "react";
 
 export default function Tweet({ tweet, username, photo, userId, id }: ITweet) {
   const user = auth.currentUser;
+  const [edit, setEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [editTweet, setEditTweet] = useState(tweet);
+  const [editPhoto, setEditPhoto] = useState<File | null>(null);
+  const [tempUrl, setTempUrl] = useState("");
+
   const onClickDelete = async () => {
     const ok = confirm("정말로 이 글을 지울까요?");
     if (!ok || user?.uid !== userId) return;
@@ -21,31 +33,118 @@ export default function Tweet({ tweet, username, photo, userId, id }: ITweet) {
       //
     }
   };
+  const onChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setEditTweet(e.target.value);
+  };
+  const onClickEdit = async () => {
+    if (user?.uid !== userId) return;
+    setEdit(true);
+  };
+  const onClickCompleteEdit = async () => {
+    try {
+      setLoading(true);
+      if (user?.uid !== userId && !edit) return;
+      const docRef = doc(database, "tweets", id);
+      await updateDoc(docRef, {
+        tweet: editTweet,
+      });
+      if (editPhoto) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user?.uid}-${user?.displayName}/${docRef.id}`
+        );
+        const result = await uploadBytes(locationRef, editPhoto);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(docRef, {
+          photo: url,
+        });
+      }
+      setEdit(false);
+      setEditPhoto(null);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const onEditFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      if (files[0].size > 1024 ** 2) {
+        alert("이미지 파일은 1MB까지 업로드할 수 있어요");
+        return;
+      }
+      setEditPhoto(files[0]);
+
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(files[0]);
+      fileReader.onload = (event) => {
+        if (typeof event.target?.result === "string") {
+          setTempUrl(event.target?.result);
+        }
+      };
+    }
+  };
   return (
     <S.Wrapper>
-      <S.Column>
-        <S.Username>{username}</S.Username>
+      <S.Username>{username}</S.Username>
+      {user?.uid === userId ? (
+        !edit ? (
+          <S.BtnWrapper>
+            <S.DeleteButton onClick={onClickDelete}>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+                />
+              </svg>
+            </S.DeleteButton>
+            <S.EditButton onClick={onClickEdit}>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+              </svg>
+            </S.EditButton>
+          </S.BtnWrapper>
+        ) : (
+          <S.CompleteEditButton onClick={onClickCompleteEdit}>
+            {loading ? "저장 중..." : "저장"}
+          </S.CompleteEditButton>
+        )
+      ) : (
+        <div></div>
+      )}
+      {!edit ? (
         <S.Payload>{tweet}</S.Payload>
-      </S.Column>
-      <S.Column>
-        {user?.uid === userId ? (
-          <S.DeleteButton onClick={onClickDelete}>
-            <svg
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-              />
-            </svg>
-          </S.DeleteButton>
-        ) : null}
-        {photo ? <S.Photo src={photo} /> : null}
-      </S.Column>
+      ) : (
+        <S.Textarea onChange={onChangeText}>{editTweet}</S.Textarea>
+      )}
+      {!photo ? null : edit ? (
+        <S.EditPhotoBox>
+          <S.EditPhotoButton htmlFor="editFile">
+            {editPhoto ? "✅" : "재업로드"}
+          </S.EditPhotoButton>
+          <S.AttachEditFileInput
+            onChange={onEditFileChange}
+            id="editFile"
+            type="file"
+            accept="image/*"
+          />
+          <S.Photo src={!editPhoto ? photo : tempUrl} />
+        </S.EditPhotoBox>
+      ) : (
+        <S.Photo src={photo} />
+      )}
     </S.Wrapper>
   );
 }
--- src/styles/tweet.styles.ts
+++ src/styles/tweet.styles.ts
@@ -1,19 +1,38 @@
 import styled from "styled-components";
+import { TextArea } from "./tweetForm.styles";
 
 export const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 3fr 0.5fr;
-  padding: 20px;
+  grid-template-rows: 30px 2fr;
+  grid-template-columns: 3fr 100px;
+  padding: 20px 0;
   border-bottom: 1px solid #dedede;
+  gap: 5px;
 `;
-export const Column = styled.div``;
 export const Username = styled.h2`
   font-weight: 600;
   font-size: 15px;
+  display: flex;
+  align-items: center;
 `;
 export const Payload = styled.p`
   margin: 10px 0;
   font-size: 18px;
+`;
+export const Textarea = styled(TextArea)`
+  height: 100%;
+`;
+export const BtnWrapper = styled.div``;
+export const EditButton = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  color: tomato;
+  border: none;
+  float: right;
+  margin-bottom: 5px;
+  svg {
+    width: 16px;
+  }
 `;
 export const DeleteButton = styled.button`
   cursor: pointer;
@@ -26,8 +45,39 @@ export const DeleteButton = styled.button`
     width: 16px;
   }
 `;
+export const CompleteEditButton = styled.button`
+  cursor: pointer;
+  width: 100px;
+  height: 30px;
+  background-color: #317a23;
+  border: none;
+  color: white;
+  border-radius: 8px;
+`;
+export const EditPhotoBox = styled.div`
+  cursor: pointer;
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+  position: relative;
+`;
+export const EditPhotoButton = styled.label`
+  cursor: pointer;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const AttachEditFileInput = styled.input`
+  display: none;
+`;
 export const Photo = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 15px;
+  object-fit: cover;
 `;
